acf324316ba419e58c0eb29708f7b6f568c07b5b
Dillon Dalton <75738116+ddalton15@users.noreply.github.com>
2021-07-20 14:20:05 -0700

    Update A_.py

--- A_.py
+++ A_.py
@@ -6,6 +6,7 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm")
 
+TURQUOISE = (64, 224, 208)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 255, 0)
@@ -15,7 +16,7 @@ BLACK = (0, 0, 0)
 PURPLE = (128, 0, 128)
 ORANGE = (255, 165 ,0)
 GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
+
 
 class Spot:
 	def __init__(self, row, col, width, total_rows):
@@ -29,45 +30,59 @@ class Spot:
 		self.total_rows = total_rows
 
 	def get_pos(self):
+		
 		return self.row, self.col
 
 	def is_closed(self):
+		
 		return self.color == RED
 
 	def is_open(self):
+		
 		return self.color == GREEN
 
 	def is_barrier(self):
+		
 		return self.color == BLACK
 
 	def is_start(self):
+		
 		return self.color == ORANGE
 
 	def is_end(self):
+		
 		return self.color == TURQUOISE
 
 	def reset(self):
+		
 		self.color = WHITE
 
 	def make_start(self):
+		
 		self.color = ORANGE
 
 	def make_closed(self):
+		
 		self.color = RED
 
 	def make_open(self):
+		
 		self.color = GREEN
 
 	def make_barrier(self):
+		
 		self.color = BLACK
 
 	def make_end(self):
+		
 		self.color = TURQUOISE
 
 	def make_path(self):
+		
 		self.color = PURPLE
 
 	def draw(self, win):
+		
 		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
 	def update_neighbors(self, grid):
@@ -89,6 +104,7 @@ class Spot:
 
 
 def h(p1, p2):
+	
 	x1, y1 = p1
 	x2, y2 = p2
 	return abs(x1 - x2) + abs(y1 - y2)
@@ -96,14 +112,16 @@ def h(p1, p2):
 
 def reconstruct_path(came_from, current, draw):
 	while current in came_from:
+		
 		current = came_from[current]
 		current.make_path()
 		draw()
 
 
 def algorithm(draw, grid, start, end):
-	count = 0
+	
 	open_set = PriorityQueue()
+	count = 0
 	open_set.put((0, count, start))
 	came_from = {}
 	g_score = {spot: float("inf") for row in grid for spot in row}
